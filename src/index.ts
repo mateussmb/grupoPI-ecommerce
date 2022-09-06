71eb95f9f27716c875943d59a4a1c0e29ec4e775
WasseLucas <wasselucas.dev@gmail.com>
2022-09-06 14:18:15 -0300

    adcionado rotas  para criar modificar e deletar um cliente por id

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import express, { Request, Response, NextFunction} from 'express';
+import express from 'express';
+import statusRoute from './routes/status.routes';
+import usersRoute from './routes/users.routes';
 
 const app = express();
+//configuração da aplicação
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+// configuração da rota
+app.use(statusRoute);
+app.use(usersRoute);
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) =>{
-   res.status(200).send({ foo: 'sucesso'}) 
-});
-
+//inicializando servidor
 app.listen(3000, ()=> {
-    console.log('Aplicação Rodando na porta 3000!')
+    console.log(
+        '----Aplicação Rodando na porta 3000!----'
+        )
 });
