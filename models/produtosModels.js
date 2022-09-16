65196cdccc246ebe42dc21cb0f94647c414df7f9
Wasse Lucas <wasselucas.dev@gmail.com>
2022-09-16 16:45:53 -0300

    implementando/Models-produto

--- models/produtosModels.js
+++ models/produtosModels.js
@@ -1,25 +1,52 @@
 
 const fs = require('fs');
-const {v4} = require('uuid');
+const { v4 } = require('uuid');
+
+const { dirname } = require("path")
+const appDirectory = dirname(require.main.filename)
 
 let bancoProdutos = require('../database/bancoProdutos.json');
 
+const { uploadPath } = require('../config/upload.js');
 
 const writeToDB = () => {
     const json = JSON.stringify(bancoProdutos);
-    fs.writeFileSync('/database/bancoProdutos', json);
+    fs.writeFileSync(`${appDirectory}/../database/bancoProdutos.json`, json);
 }
+
+// Estudar sobre estrutura de repositories com banco de dados
 
 const Produto = {
     findAll:() => bancoProdutos.produtos,
 
-    create:(registroProdutos, avatar) =>{
-        bancoProdutos.produtos.push({id: v4(), ...registroProdutos, avatar });
+    findById:(id) => {
+        const produto = bancoProdutos.produtos.find(produto => produto.id ===id);
+        return produto;
+    },
+
+    removeAvatar:() =>{
+        const produto = bancoProdutos.produtos.find(produto => produto.id === id)
+        fs.unlinkSync(
+            `${uploadPath}/${produto, avatar}`
+        );
+    },
+    
+    create:(produto, avatar) =>{
+        const createdProduct = {id: v4(), ...produto, avatar };
+        bancoProdutos.produtos.push(createdProduct);
         writeToDB();
+        return createdProduct;
+    },
+
+    update:(id, produto, avatar)=>{
+        const produtoIndex = bancoProdutos.produtos.produtoIndex(produto => produto.id === id);
+        bancoProdutos.produtos[produtoIndex] = {id, ...produto, avatar};
+        writeToDB 
     },
     delete:(id) => {
-        const produtoIndex = bancoProdutos.produtos.findIndex(registroProdutos => produtos.id===id);
-        bancoProdutos.produtos[produtoIndex] = {id , ... produto};
+        const produtoIndex = bancoProdutos.produtos.findIndex(produto => produto.id===id);
+        bancoProdutos.produtos.splice(produtoIndex, 1);
+        writeToDB
     }
 }
 
