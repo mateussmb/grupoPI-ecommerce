0d16f945c0f1ec1423701f3044b553b25e332640
Wasse Lucas <wasselucas.dev@gmail.com>
2022-09-30 14:15:07 -0300

    alterando rota e controlller login-ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const homeController = require('../controllers/homeController');
 const pagamentoController = require('../controllers/pagamentoController');
 const carrinhoController = require('../controllers/carrinhoController');
 const checkoutController = require('../controllers/checkoutController');
-const loginCadastro = require('../controllers/login-cadastro');
+const  loginController = require('../controllers/loginController');
 
 var router = express.Router();
 
@@ -11,6 +11,6 @@ router.get('/', homeController.index);
 router.get('/pagamento', pagamentoController.index);
 router.get('/carrinho', carrinhoController.index);
 router.get('/checkout', checkoutController.index);
-router.get('/login', loginCadastro.index);
+router.get('/login', loginController.index);
 
 module.exports = router;
