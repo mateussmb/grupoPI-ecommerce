50e72edd31ceeec2cc50d43cc1117e3c406c5c25
Wasse Lucas <wasselucas.dev@gmail.com>
2022-10-05 12:59:50 -0300

    userAuthentication

--- middlewares/usersAuthentication.js
+++ middlewares/usersAuthentication.js
@@ -1,13 +1,18 @@
+// ==================================================
+// Middleware para verificar se o usuário está logado
+// E permitir o acesso a páginas restritas
+// ==================================================
 
-const userAuthenticated = (req, res, next) => {
-
-    if(req.session.user == undefined){
-
-        return res.redirect('/login')
+const userIsAuthenticated = (req, res, next) => {
+    // Verifica se o usuário está logado
+    // Ou seja, se existe uma sessão para o usuário
+    if (req.session.user == undefined) {
+      // Se não estiver logado, redireciona para a página de login
+      return res.redirect('/login');
     }
-
-    //se usuario tiver logado continua execução
+    
+    // Se estiver logado, continua a execução
     next();
-}
-
-module. exports =  userAuthenticated;
+  }
+  
+  module.exports = userIsAuthenticated;
