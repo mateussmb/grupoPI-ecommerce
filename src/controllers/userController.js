e7c93addb6587a33b3c43129bff080ab0dc57165
Wasse Lucas <wasselucas.dev@gmail.com>
2022-10-09 13:52:16 -0300

    implementando models rotas e controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,21 @@
+const { validationResult } = require('express-validator');
+
 const userController = {
-    formAddUser:(req, res) =>{
+    /*
+    *Renderizando para de cadastro de usuario
+    */
+    viewCadastro:(req, res) =>{
     res.render("pages/userRegistration")
-}
+    },
+    salvarCadastro:(req, res) => {
+        //validaçãa de erros
+        if(!errors.insEmpty()){
+            return res.render("pages/userRegistration",{errors});
+        }
+        if(!req.insEmpty){
+            return res.send('Você deve preencher')
+        }
+    }
+
 }
 module.exports = userController
